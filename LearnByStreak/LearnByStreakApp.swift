32ec90b23291f3f3f3a87eeb5a304cf5827c448a
ruba <rub.gmd23@gmail.com>
2025-10-19 12:34:02 +0300

    Initial Commit

---
+++ LearnByStreak/LearnByStreakApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnByStreakApp.swift
+//  LearnByStreak
+//
+//  Created by Ruba Alghamdi on 27/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct LearnByStreakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LearnByStreakTests/LearnByStreakTests.swift
@@ -0,0 +1,17 @@
+//
+//  LearnByStreakTests.swift
+//  LearnByStreakTests
+//
+//  Created by Ruba Alghamdi on 27/04/1447 AH.
+//
+
+import Testing
+@testable import LearnByStreak
+
+struct LearnByStreakTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
